66f00d77affa3b0940d297a7f185647246573442
kavindaHG <kavinda731@gmail.com>
2022-12-08 13:18:22 +0530

    initial commit

---
+++ src/main/java/com/kavinda/employeeSystemBE/EmployeeSystemBeApplication.java
@@ -0,0 +1,13 @@
+package com.kavinda.employeeSystemBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSystemBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeSystemBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kavinda/employeeSystemBE/EmployeeSystemBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kavinda.employeeSystemBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeSystemBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
